dec6b22ee6a358b43e03879ac556511faa744e33
Mr111222 <Mr_zhouzhi@163.com>
2018-08-28 16:41:21 +0800

    简单的vue-pro搭建

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="lists">
+  	<p>我是list</p>
+  	<ul>
+  		<li v-for="(i,index) in lists" :key="index">
+  			{{i.text}}
+  		</li>
+  	</ul>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+	import axios from "axios"
+	export default {
+	  data() {
+	    return {
+	      lists: []
+	    };
+	  },
+	  created() {
+	    this.$http.get('/lists').then((res) => {
+	    	console.log(res.data)
+	        this.lists = res.data.data;
+	    });
+	  }
+	};
+</script>
